72378b8033dca3d81f0ac0ccdd65e5484e45212c
CoderSales <32943259+CoderSales@users.noreply.github.com>
2024-05-13 23:38:40 +0100

    Add sites to manifest json Add console logs to popup js

--- tabs-manager/popup.js
+++ tabs-manager/popup.js
@@ -2,14 +2,56 @@ const tabs = await chrome.tabs.query({
     url: [
       "https://developer.chrome.com/docs/webstore/*",
       "https://developer.chrome.com/docs/extensions/*",
+      "https://github.com/*",
+      "https://stackoverflow.com/*",
+      "https://www.w3schools.com/*"
     ]
 });
+
+for (let i=0;i<tabs.length;i++) {
+    console.log(tabs[i].url);
+}
 
 const collator = new Intl.Collator();
 tabs.sort((a, b) => collator.compare(a.title, b.title));
 
 const template = document.getElementById("li_template");
 const elements = new Set();
+
+console.log("line 17: template:", template);
+console.log("line 18: elements:", elements);
+
+
+const iterator2 = elements.entries();
+
+console.log(iterator2);
+
+console.log("hi4");
+
+console.log("line 25: elements.entries()",elements.entries());
+const entries = elements.entries();
+console.log("line 27:", entries);
+for (const entry of entries) {
+    console.log("line 29:", entry);
+}
+console.log("line 31: hi");
+
+
+
+
+
+
+const iterator3 = elements[Symbol.iterator]();
+
+console.log(iterator3.next().value);
+
+console.log(iterator3.next().value);
+
+
+
+
+
+
 
 for (const tab of tabs) {
     const element = template.content.firstElementChild.cloneNode(true);
